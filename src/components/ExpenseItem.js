6ef1375ebd3b872d6f6b6ff9524a39e634d5d260
balaji24k <balaji325s@gmail.com>
2023-01-26 22:56:41 +0530

    Task9React1Added

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,13 @@
 import './ExpenseItem.css';
-function ExpenseItem() {
-    const expenseDate = new Date(2021, 2, 28);
-    const expenseTitle = 'Car Insurance';
-    const expenseAmount = 294.67;
-    const LocationOfExpenditure = 'Vizag'
+function ExpenseItem(props) {
+    
     return (
     <div className='expense-item'>
         <div className='expense-item__description'>
-            <div>{expenseDate.toISOString()}</div>
-            <h2>{expenseTitle}</h2>
-            <h2>{LocationOfExpenditure}</h2>
-            <div className='expense-item__price'> ${expenseAmount}</div>
+            <div>{props.item.date.toISOString()}</div>
+            <h2>{props.item.title}</h2>
+            <h2>{props.item.locationOfExpenditure}</h2>
+            <div className='expense-item__price'> ${props.item.amount}</div>
         </div>
     </div>
     );
