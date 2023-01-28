0cc2ac382431c9085ba9a618abdbc709b0257952
balaji24k <balaji325s@gmail.com>
2023-01-28 19:37:46 +0530

    Task12React1Added

--- src/App.js
+++ src/App.js
@@ -1,43 +1,56 @@
-import ExpenseItem from './components/ExpenseItem'
+// import ExpenseItems from "./components/ExpenseItems";when we use maps
+// import React from 'react'; if we write code in react object
+import Expenses from "./components/ExpenseList/Expenses";
 
-function App() {
-  const expenses = [
+const App = () => {
+  const expensesData = [
     {
-      id: 'e1',
-      title: 'Toilet Paper',
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-      locationOfExpenditure: "Vizag",
-    },
-    { id: 'e2', 
-    title: 'New TV', 
-    amount: 799.49, 
-    date: new Date(2021, 2, 12),
-    locationOfExpenditure: "Chennai",
-   },
-    {
-      id: 'e3',
-      title: 'Car Insurance',
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-      locationOfExpenditure: "Goa",
+      id: "el",
+      name: "Food",
+      amount: 500,
+      date: new Date(2022, 7, 15),
+      LocationOfExpenditure: "Restaurent Hyd",
     },
     {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
-      amount: 450,
-      date: new Date(2021, 5, 12),
-      locationOfExpenditure: "Delhi",
+      id: "bl",
+      name: "Shopping",
+      amount: 5000,
+      date: new Date(2022, 10, 12),
+      LocationOfExpenditure: "Trends",
+    },
+    {
+      id: "ml",
+      name: "Movie",
+      amount: 1000,
+      date: new Date(2022, 6, 13),
+      LocationOfExpenditure: "Imax Hyd",
+    },
+    {
+      id: "cl",
+      name: "Tour",
+      amount: 20000,
+      date: new Date(2022, 4, 12),
+      LocationOfExpenditure: "goa trip",
+    },
+    {
+      id: "dl",
+      name: "voccation",
+      amount: 30000,
+      date: new Date(2022, 6, 12),
+      LocationOfExpenditure: "bangkok",
     },
   ];
+
   return (
     <div>
-      <h2>Let's get started!</h2>
-      {expenses.map((expense) => (
-        <ExpenseItem item={expense}
-        ></ExpenseItem>
-      ))}
-
+      <h1>let's start</h1>
+      <h2>ExpenseItems Details</h2>
+      {/* {expenses.map((any) => (
+        <ExpenseItems data={any}></ExpenseItems>
+      ))} */}
+      <Expenses expenses={expensesData} />
+      {/* return React.createElement('div',{},
+  React.createElement(Expenses,{items:expenses})); react object code */}
     </div>
   );
 }
