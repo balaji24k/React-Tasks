c6db5c995cb77a102864342af7b07fc16c8874b5
balaji24k <balaji325s@gmail.com>
2023-02-04 00:07:33 +0530

    Task14React2Added

--- src/components/ExpenseList/Expenses.js
+++ src/components/ExpenseList/Expenses.js
@@ -4,6 +4,7 @@ import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import { useState } from "react";
 import ExpensesList from "./ExpensesList";
+import ExpenseChart from "./ExpensesChart";
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear]=useState('2020');
@@ -23,7 +24,7 @@ const Expenses = (props) => {
        selected={filteredYear} 
        onChangeFilter={filterChangeHandler}
     />
-
+    <ExpenseChart expenses={filteredExpenses}/>
     <ExpensesList items = {filteredExpenses}/>
     
     </Card>
