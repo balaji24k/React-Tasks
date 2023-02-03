73fe680bb028ae16d5d2c26b16465888d0cbcbe1
balaji24k <balaji325s@gmail.com>
2023-02-03 15:25:12 +0530

    Task9React2Added

--- src/components/ExpenseList/Expenses.js
+++ src/components/ExpenseList/Expenses.js
@@ -1,10 +1,18 @@
 import "./Expenses.css";
 import ExpenseItems from "./ExpenseItems";
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
+import { useState } from "react";
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear]=useState('2020');
+
+  const filterChangeHandler = selectedYear => {
+    setFilteredYear(selectedYear);
+  }
   return (
     <Card className="expenses">
+      <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
       <ExpenseItems
         name={props.expenses[0].title}
         amount={props.expenses[0].amount}
@@ -29,6 +37,13 @@ const Expenses = (props) => {
         date={props.expenses[3].date}
         LocationOfExpenditure={props.expenses[3].LocationOfExpenditure}
       ></ExpenseItems>
+
+      {/* <ExpenseItems
+        name={props.expenses[4].title}
+        amount={props.expenses[4].amount}
+        date={props.expenses[4].date}
+        // LocationOfExpenditure={props.expenses[3].LocationOfExpenditure}
+      ></ExpenseItems> */}
     
     </Card>
   );
